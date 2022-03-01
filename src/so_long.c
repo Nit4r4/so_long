6c9f224c71d66ac6b7e31ef91ac463effc5a8e2f
Verena Ferraro <vferraro@c1r5s6.42lausanne.ch>
2022-03-01 14:19:43 +0100

    01.03.22

--- src/so_long.c
+++ src/so_long.c
@@ -6,18 +6,41 @@
 /*   By: vferraro <vferraro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 15:24:50 by vferraro          #+#    #+#             */
-/*   Updated: 2022/02/22 15:25:14 by vferraro         ###   ########.fr       */
+/*   Updated: 2022/03/01 14:13:54 by vferraro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./mlx/mlx.h"
 
+typedef struct  s_data 
+{
+    void    *img;
+    void    *addr;
+    int     bits_per_pixel;
+    int     line_lenght;
+    int     endian;
+}               t_data;
+
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char *dst;
+
+    dst = data->addr+ (y * data->line_lenght + x * (data->bits_per_pixel/ 8));
+    *(unsigned int*)dst= color;
+}
+
 int main(void)
 {
     void    *mlx;
     void    *mlx_win;
+    //void    *img;
+    t_data  img;
 
     mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello World !");
-    mlx_loop(mlx); 
+    mlx_win = mlx_new_window(mlx, 400, 400, "N1t4r4");
+    img.img = mlx_new_image(mlx, 400, 400);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
+    my_mlx_pixel_put(&img, 200, 200, 0x0000FF00);
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_loop(mlx);
 }
